c9862666de63d25a113170bab080dc8e9f3ab4f6
Joao Araujo <joaovictorclassroom@gmail.com>
2024-03-04 18:21:23 -0300

    comentando bonitinho

--- app/src/main/java/com/aula/jogodavelha/MainActivity.java
+++ app/src/main/java/com/aula/jogodavelha/MainActivity.java
@@ -20,24 +20,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Iniciando variáveis ao criar a tela
         jogo = new char[3][3];
         jogada = 0;
         handler = new Handler();
     }
+
     public boolean verificarVitoria(){
+        /*
+         * Percorre a matriz e retorna um boolean indicando se o jogo acabou ou não.
+         */
+
+        // laço que verifica linhas e colunas pra ver se alguém ganhou
         for(int i = 0; i < jogo.length; i++){
             if((jogo[i][0] == jogo[i][1] && jogo[i][1] == jogo[i][2] && (jogo[i][0] == 'x' || jogo[i][0] == 'o')) ||
                 (jogo[0][i] == jogo[1][i] && jogo[1][i] == jogo[2][i] && (jogo[0][i] == 'x' || jogo[0][i] == 'o')) ){
                 return true;
             }
         }
+        
+        // verifica as diagonais
         return ((jogo[0][0] == jogo[1][1] && jogo[1][1] == jogo[2][2]) && (jogo[0][0] == 'x' || jogo[0][0] == 'o'))
                 || ((jogo[0][2] == jogo[1][1] && jogo[1][1] == jogo[2][0]) && (jogo[0][2] == 'x' || jogo[0][2] == 'o'));
     }
 
     public void limparJogo(){
+        /*
+         * Limpa o jogo, reiniciando todas as variáveis e jogadas da rodada passada.
+         */
+
+        // limpa a matriz
         jogo = new char[3][3];
+
+        // reinicia contador de jogada
         jogada = 0;
+
+        // limpa o tabuleiro
         ((ImageView) findViewById(R.id.a00)).setImageDrawable(new ColorDrawable(Color.TRANSPARENT));
         ((ImageView) findViewById(R.id.a01)).setImageDrawable(new ColorDrawable(Color.TRANSPARENT));
         ((ImageView) findViewById(R.id.a02)).setImageDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -47,40 +66,93 @@ public class MainActivity extends AppCompatActivity {
         ((ImageView) findViewById(R.id.a20)).setImageDrawable(new ColorDrawable(Color.TRANSPARENT));
         ((ImageView) findViewById(R.id.a21)).setImageDrawable(new ColorDrawable(Color.TRANSPARENT));
         ((ImageView) findViewById(R.id.a22)).setImageDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        // seta o texto como vazio
         ((TextView) findViewById(R.id.resultado)).setText(" ");
     }
 
     public void colocarJogada(View view) {
+        /*
+         * Recebe uma view com a posição jogada. 
+         * Adiciona foto da jogada na tela principal e verifica se alguém ganhou.
+         */
+
+        // pega a jogada
         ImageView posicao = (ImageView) view;
+
+        // pega o id da jogada
         String id = view.getResources().getResourceEntryName(posicao.getId()).replace("a", "");
+
+        // inicializando index da matriz
         int i;
         int j;
+
+        // verifica se view que chamou a função não é a do background
         if(!id.contains("b")){
+            
+            // pega o index da matriz pelo id
             i = Integer.parseInt(String.valueOf(id.charAt(0)));
             j = Integer.parseInt(String.valueOf(id.charAt(1)));
         }else{
+
+            // encerra a execução da função
             return;
         }
+
+        // verifica se a posição da jogada é nula na matriz
         if(jogo[i][j] == '\u0000'){
+            
+            // se a jogada for par, é jogada do X
             if(jogada % 2 == 0) {
+
+                // seta a foto como a foto do X
                 posicao.setImageResource(R.drawable.x);
+
+                // preenche a posição com X na matriz
                 jogo[i][j] = 'x';
-            }else{
+            }
+            
+            // se a jogada for ímpar, é jogada do O
+            else{
+
+                // seta a foto como a foto do O
                 posicao.setImageResource(R.drawable.o);
+
+                // preenche a posição com O na matriz
                 jogo[i][j] = 'o';
             }
+
+            // incrementa a jogada
             jogada++;
+
+            // verifica se algúem ganhou
             if(verificarVitoria()){
+
+                // seta o texto como vitória
                 ((TextView) findViewById(R.id.resultado)).setText("Vitória!!!!");
+
+                // seta jogada como 9, para sinalizar que acabou o jogo
                 jogada = 9;
-            } else if (jogada == 9) {
+            } 
+            
+            // verifica ninguém ganhou e a jogada é 9
+            else if (jogada == 9) {
+                
+                // seta o texto como empate
                 ((TextView) findViewById(R.id.resultado)).setText("Empate...");
             }
+
+            // verifica se o jogo acabou e reinicia
             if(jogada == 9){
+
                 handler.postDelayed(() -> {
+
+                    // seta o texto como reiniciando com delay
                     ((TextView) findViewById(R.id.resultado)).setText("Reiniciando!!");
-                    handler.postDelayed(this::limparJogo, 2000
-                    );
+
+                    // limpa o jogo com delay
+                    handler.postDelayed(this::limparJogo, 2000);
+
                 }, 2000);
             }
         }
--- app/src/main/java/com/aula/jogodavelha/TelaInicial.java
+++ app/src/main/java/com/aula/jogodavelha/TelaInicial.java
@@ -10,12 +10,17 @@ public class TelaInicial extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
         Handler handler = new Handler();
-        handler.postDelayed(this::abrirSegundaTelas, 3000);
+
+        // chama a tela principal com delay
+        handler.postDelayed(this::abrirTelaPrincipal, 3000);
     }
 
-    private void abrirSegundaTelas() {
+    private void abrirTelaPrincipal() {
         Intent intent = new Intent(TelaInicial.this, MainActivity.class);
+
+        // inicia a tela principal
         startActivity(intent);
+        
         finish();
     }
 }
